a0c96906600073b29cca4b6153e2405c10e5c1f6
HTunne <py13ht@leeds.ac.uk>
2020-09-11 00:01:58 +0100

    Complete move to TaskList.

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-list-item :value="task.uuid">
+    <v-list-item-avatar v-if="task.status === 'completed'" color="error">{{ task.id }}</v-list-item-avatar>
+    <v-list-item-avatar v-else-if="task.start" color="success">{{ task.id }}</v-list-item-avatar>
+    <v-list-item-avatar v-else color="grey">{{ task.id }}</v-list-item-avatar>
+    <v-list-item-content>
+      <v-btn depressed small outlined left color="primary" v-if="'project' in task" v-text="task.project" class="project-btn text-none font-weight-bold" @click.stop="search = 'p:' + task.project">
+      </v-btn>
+      <v-list-item-subtitle v-text="task.description">
+      </v-list-item-subtitle>
+      <v-item-group v-if="'tags' in task">
+        <v-btn
+          v-for="tag in task.tags"
+          rounded
+          x-small
+          depressed
+          style="margin: 0 5px 5px 0"
+          color="primary"
+          :key="tag"
+          @click.stop="search = '+' + tag"
+          class="text-none">
+            {{ tag }}
+        </v-btn>
+      </v-item-group>
+    </v-list-item-content>
+    <v-list-item-avatar class="urgency-avatar" v-text="task.urgency"></v-list-item-avatar>
+    <v-list-item-action>
+      <v-btn fab depressed><v-icon>mdi-check</v-icon></v-btn>
+    </v-list-item-action>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'taskItem',
+  props: {
+    task: Object
+  }
+}
+</script>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -46,50 +46,24 @@
         <v-list three-line class="pa-0">
           <v-list-item-group active-class="green lighten-5" v-model="selectedTaskUUID" @change="onSelectionChange($event)">
             <template v-for="(task, index) in props.items">
-              <v-list-item :key="task.uuid" :value="task.uuid">
-                <v-list-item-avatar v-if="task.status === 'completed'" color="error">{{ task.id }}</v-list-item-avatar>
-                <v-list-item-avatar v-else-if="task.start" color="success">{{ task.id }}</v-list-item-avatar>
-                <v-list-item-avatar v-else color="grey">{{ task.id }}</v-list-item-avatar>
-                <v-list-item-content>
-                  <v-btn depressed small outlined left color="primary" v-if="'project' in task" v-text="task.project" class="project-btn text-none font-weight-bold" @click.stop="search = 'p:' + task.project">
-                  </v-btn>
-                  <v-list-item-subtitle v-text="task.description">
-                  </v-list-item-subtitle>
-                  <v-item-group v-if="'tags' in task">
-                    <v-btn
-                      v-for="tag in task.tags"
-                      rounded
-                      x-small
-                      depressed
-                      style="margin: 0 5px 5px 0"
-                      color="primary"
-                      :key="tag"
-                      @click.stop="search = '+' + tag"
-                      class="text-none">
-                        {{ tag }}
-                    </v-btn>
-                  </v-item-group>
-                </v-list-item-content>
-                <v-list-item-avatar class="urgency-avatar" v-text="task.urgency"></v-list-item-avatar>
-                <v-list-item-action>
-                  <v-btn fab depressed><v-icon>mdi-check</v-icon></v-btn>
-                </v-list-item-action>
-              </v-list-item>
-
-          <v-divider v-if="index + 1 < tasksSorted.length" :key="index"></v-divider>
-        </template>
-      </v-list-item-group>
-    </v-list>
-    </template>
-  </v-data-iterator>
+              <taskItem :task="task" :key="task.uuid"/>
+              <v-divider v-if="index + 1 < tasksSorted.length" :key="index"></v-divider>
+            </template>
+          </v-list-item-group>
+        </v-list>
+      </template>
+    </v-data-iterator>
   </v-container>
 </template>
 
 <script>
+import taskItem from '@/components/TaskItem.vue';
+
 export default {
-name: 'taskList',
-          components: {
-          },
+  name: 'taskList',
+  components: {
+    taskItem
+  },
 computed: {
   selectedTaskUUID: {
     get: function () {
