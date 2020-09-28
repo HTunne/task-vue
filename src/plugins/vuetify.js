cced4993be52394945c5a99f50de6b09d21f8443
HTunne <py13ht@leeds.ac.uk>
2020-09-28 02:57:30 +0100

    Add theme colours and mobile breakpoint.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    breakpoint: {
+        mobileBreakpoint: 'xs'
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: '#156D7E',
+                accent: '#EB6B96',
+                secondary: '#929596',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252'
+            }
+        }
+    }
 });
