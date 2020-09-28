164a9f89b1b235e2cf7022e862cd3e4938361098
HTunne <py13ht@leeds.ac.uk>
2020-09-28 02:59:38 +0100

    Move child views to overlay for mobile view.

--- src/views/TaskList.vue
+++ src/views/TaskList.vue
@@ -6,7 +6,7 @@
   <v-row no-gutters
     style="height: 100%"
   >
-    <v-col class="main-col">
+    <v-col class="main-col" v-if="!$vuetify.breakpoint.mobile">
       <div>
           <v-alert text dismissible :type="alert.type" v-for="alert in alerts" :key="alert.message">
               {{ alert.description }}
@@ -15,11 +15,30 @@
       <router-view></router-view>
     </v-col>
     <v-col cols=12 sm=5 lg=4 xl=3
-      style="height: 100%"
+      class="main-col"
     >
       <taskList :tasks="tasks" type="pending"/>
     </v-col>
   </v-row>
+  <v-overlay v-if="$vuetify.breakpoint.mobile" :value="!noneSelected" >
+      <v-container class="main-overlay">
+          <v-row no-gutters>
+          <v-col cols = 12>
+              <v-btn
+                   @click="onCloseOverlay"
+                   color="accent"
+                   small
+                   class="float-right"
+                   fab>
+                  <v-icon>mdi-close</v-icon>
+              </v-btn>
+          </v-col>
+          <v-col>
+        <router-view></router-view>
+          </v-col>
+          </v-row>
+      </v-container>
+  </v-overlay>
   </v-container>
 </template>
 
@@ -46,10 +65,21 @@ export default {
         },
         recurPage() {
             return this.$route.meta.recurPage
+        },
+        noneSelected() {
+            return !!this.$route.meta.noneSelected
         }
     },
     methods: {
-        ...mapActions(["fetchTaskList"])
+        ...mapActions(["fetchTaskList"]),
+        onCloseOverlay() {
+            this.$store.commit('set_selected_task_uuid', undefined);
+            if (this.recurPage) {
+                this.$router.push({ name: 'TaskRecurNoneSelected' });
+            } else {
+                this.$router.push({ name: 'TaskNoneSelected' });
+            }
+        }
     },
     created() {
         this.fetchTaskList();
@@ -61,5 +91,10 @@ export default {
 .main-col {
     overflow-y: scroll;
     height: 100%;
+}
+
+.main-overlay {
+    overflow-y: scroll;
+    height: 100vh;
 }
 </style>
