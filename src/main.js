c10a010bb8e56644dc09195c239a66e9e22e92c0
HTunne <py13ht@leeds.ac.uk>
2020-09-29 12:52:54 +0100

    Add pwa and icons.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
