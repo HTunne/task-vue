a811d8ff3168b542d63aceb5c3a12a4c68f9fa8e
HTunne <py13ht@leeds.ac.uk>
2020-10-02 16:56:26 +0100

    Build.

---
+++ precache-manifest.1a7ec38018d93c7ccc1247c936d9219a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960fa49a0eb38da56633",
+    "url": "/task-vue/css/app.2fb251ca.css"
+  },
+  {
+    "revision": "78c5c883b60dbe7153c3",
+    "url": "/task-vue/css/chunk-vendors.ba07be32.css"
+  },
+  {
+    "revision": "5554db5848d794d7d303688e3f2b81d0",
+    "url": "/task-vue/index.html"
+  },
+  {
+    "revision": "cc46813c1269f4bde7be",
+    "url": "/task-vue/js/about.9f328133.js"
+  },
+  {
+    "revision": "960fa49a0eb38da56633",
+    "url": "/task-vue/js/app.03016627.js"
+  },
+  {
+    "revision": "78c5c883b60dbe7153c3",
+    "url": "/task-vue/js/chunk-vendors.776acde9.js"
+  },
+  {
+    "revision": "150d90190c4c2faaf6ff3bc183b23347",
+    "url": "/task-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/task-vue/robots.txt"
+  }
+]);
